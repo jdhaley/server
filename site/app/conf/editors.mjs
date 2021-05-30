e4151d8d062c9bf6a07ec0649787e429c78f8631
John Haley <jdhaley@gmail.com>
2021-05-29 23:43:37 -0400

    editors cleaned up.

--- site/app/conf/editors.mjs
+++ site/app/conf/editors.mjs
@@ -14,6 +14,12 @@ export default {
 		this.peer.type = "date";
 		if (this.conf.readOnly) this.peer.setAttribute("disabled", true);
 	},
+	boolean() {
+		this.let("nodeName", "input");
+		this.peer.type = "checkbox";
+		if (this.conf.readOnly) this.peer.setAttribute("disabled", true);
+	},
+	//the following should not be specified as a datatype, only as an inputType.
 	dateTime() {
 		this.let("nodeName", "input");
 		this.peer.type = "datetime";
@@ -24,11 +30,12 @@ export default {
 		this.peer.type = "color";
 		if (this.conf.readOnly) this.peer.setAttribute("disabled", true);
 	},
-	boolean() {
+	password() {
 		this.let("nodeName", "input");
-		this.peer.type = "checkbox";
+		this.peer.type = "password";
 		if (this.conf.readOnly) this.peer.setAttribute("disabled", true);
 	},
+	//more work needed...
 	object: link,
 	array: link,
 	link: link
@@ -41,80 +48,4 @@ function link() {
 	editor.classList.add("link");
 	editor.setAttribute("href", "");
 	editor.textContent = "...";
-}
-//	text: {
-//		type$: "use.prop.Text"
-//	},
-//	action: {
-//		type$: "use.prop.Text"
-//	},
-//	media: {
-//		type$: "use.prop.Media"
-//	},
-//	link: link,
-//	list: {
-//		type$: "use.prop.Link",
-//		listType: "list | table | set"
-//	}
-
-
-function scalar() {
-	let control = this;
-	let editor;
-	if (control.conf.protected) {
-		editor = control.owner.createNode("input");
-		editor.type = "password";
-	} else if (control.conf.input) {
-		editor = control.owner.createNode("input");
-		editor.type = inputType(control);
-	} else {
-		editor = control.owner.createNode("div");
-		editor.classList.add("input");
-		editor.type = this.conf.dataType;
-		editor.contentEditable = true;
-	}
-	editor.classList.add("widget");
-	return editor;
-}
-
-function inputType(control) {
-	let type = control.conf.dataType || typeof control.model;
-	switch (type) {
-		case "number":
-			return "number";
-		case "date":
-			return "date";
-		case "boolean":
-			return "checkbox";
-		default:
-			return "text";
-	}
-}
-//"color"
-const types = ["", "other", "string", "number", "boolean", "date", "array", ];
-function typeOf(value) {
-	//'empty' values
-	if (value === undefined || value === null || isNaN(value)) return "";
-	
-	let type = typeof value;
-	switch (type) {
-		case "string":
-			//parse date string
-			//parse color
-			//parse url?
-			return type;
-		case "number":
-		case "boolean":
-			return type;
-		case "bigint":
-			return "number";
-		case "symbol":
-		case "function":
-			return "other";
-	}
-	
-	if (value instanceof Date) return "date";
-	if (value[Symbol.iterable]) return typeof value.length == "number" ? "array" : "sequence";
-	let proto = Object.getPrototypeOf(value);
-	if (proto === null || proto === Object.prototype) return "object";
 }
