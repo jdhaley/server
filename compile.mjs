ecd23d83d174b18ffd90878f6f460fc43812ef55
John Haley <jdhaley@gmail.com>
2021-06-07 08:25:54 -0400

    server/compile devt

--- compile.mjs
+++ compile.mjs
@@ -1,20 +1,25 @@
 
+import { log } from "console";
 import fs from "fs";
 
 let module = {
     id: "base.youni.works",
     version: "1.2.0"
 };
-compile("fs/source/", module);
+compile("source/", module);
 console.log(module);
 
 async function compile(sourceDir, module) {
     module.packages = {};
-    let dir = fs.readdirSync(sourceDir + module.id);
+    let dir = fs.readdirSync(sourceDir + module.id + "/packages");
     for (let fname of dir) {
-        let name = fname.substring(0, fname.lastIndexOf("."));
-        fname = "./" + sourceDir + module.id + "/" + fname;
-        module.packages[name] = (await import(fname)).default;
+        let index = fname.lastIndexOf(".");
+        let name = fname.substring(0, index);
+        let ext = fname.substring(index + 1);
+        if (ext == "mjs") {
+            fname = "./" + sourceDir + module.id + "/packages/" + fname;
+            module.packages[name] = (await import(fname)).default;    
+        }
     }
 }
 
---
+++ source/base.youni.works/conf.mjs
@@ -0,0 +1,8 @@
+export default {
+    id: "base.youni.works",
+    version: "1.2.0",
+    moduleType: "library",
+    uses: {
+        system: "system.youni.works-2.1"
+    }
+}
