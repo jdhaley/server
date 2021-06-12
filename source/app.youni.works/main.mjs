a50e6a6f31cb1ea4f0863e6d8ba2bb4e6be2aa7d
John Haley <jdhaley@gmail.com>
2021-06-12 12:26:45 -0400

    app.main - fixed  issues due to new changes.

--- source/app.youni.works/main.mjs
+++ source/app.youni.works/main.mjs
@@ -1,7 +1,8 @@
 export default function main(module, conf) {
+	conf.window = window;
 	let sys = module.use.ui.use.system.sys;
 	module = sys.load(module);
 	let app = sys.extend(module.package.app.App);
-	app.start(conf.app);
+	app.start(conf);
 	return module;
 }
