9c8bd79fc74a1947ba6f73ab7bdcce02953cf221
John Haley <jdhaley@gmail.com>
2021-06-27 19:24:06 -0400

    _snip removed from server

--- _snip/site/app/conf/editors.mjs
+++
@@ -1,9 +0,0 @@
-export default {
-	type$string: "/ui.youni.works/editors/String",
-	type$number: "/ui.youni.works/editors/Number",
-	type$date: "/ui.youni.works/editors/Date",
-	type$boolean: "/ui.youni.works/editors/Boolean",
-	type$link: "/ui.youni.works/editors/Link",
-	//the following should not be specified as a datatype, only as an inputType.
-	type$color: "/ui.youni.works/editors/Color"
-}
