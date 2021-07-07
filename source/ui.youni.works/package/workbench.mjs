a5d0b45a9b45a76217a911e9a93dc0f4ffb04a73
John Haley <jdhaley@gmail.com>
2021-07-07 11:28:16 -0400

    workbench devt

--- source/ui.youni.works/package/workbench.mjs
+++ source/ui.youni.works/package/workbench.mjs
@@ -9,7 +9,7 @@ export default {
             },
 			sidebar: {
                 type$: "Collection",
-                type$contentType: "Section"
+                type$contentType: "Directory"
             },
 			main: {
                 type$: "Structure",
@@ -17,9 +17,44 @@ export default {
                     tabs: {
                         type$: "Display"
                     },
-
                 }
             }
+		}
+    },
+    Directory: {
+        type$: "Section",
+        view(model) {
+            this.super(view, model);
+            this.parts.header.peer.textContent = this.key;
+        },
+        size(x, y) {
+			// for (let part of this.to) {
+			// 	if (part != this.parts.body) y -= part.bounds.height;
+			// }
+			// this.style.minWidth = x + "px";
+            // this.style.maxWidth = x + "px";
+			// this.parts.body.style.minHeight = y + "px";
+			// this.parts.body.style.maxHeight = y + "px";
+		},
+		extend$actions: {
+			collapse(event) {
+				if (this.collapsed === "false") {
+					this.parts.body.style.display = "none";
+					this.parts.body.style.maxHeight = "0";
+					this.collapsed = "true";
+				}
+			},
+			expand(event) {
+				if (this.collapsed === "true") {
+					this.parts.body.style.removeProperty("display");
+					this.collapsed = "false";
+				}
+			},
+			click(event) {
+				if (event.target == this.parts.header.peer) {
+					this.receive(this.collapsed === "true" ? "expand" : "collapse");
+				}
+			}
 		}
     }
 }
