0d5cd4dd40c183f75fd2c747d0eefd1a2283792f
John Haley <jdhaley@gmail.com>
2021-07-14 10:56:24 -0400

    added footer, etc. to workbench

--- source/ui.youni.works/package/workbench.mjs
+++ source/ui.youni.works/package/workbench.mjs
@@ -2,82 +2,102 @@ export default {
 	type$: "/panel",
     type$Shape: "/shape/Shape",
 	Workbench: {
+        type$: "Section",
+		direction: "horizontal",
+		members: {
+            type$header: "Display",
+			type$body: "WorkbenchBody",
+			type$footer: "WorkbenchFooter"
+		}
+    },
+    WorkbenchFooter: {
+        type$: "Display"
+    },
+	WorkbenchBody: {
         type$: "Structure",
 		direction: "horizontal",
 		members: {
-			context: {
-                type$: "Display"
-            },
-			sidebar: {
-                type$: ["Structure", "Shape"],
-                members: {
-                    tree: {
-                        type$: "Collection",
-                        type$contentType: "Item"
-                    },
-                    value: {
-                        type$: "Display",
-                        nodeName: "pre",
-                        extend$actions: {
-                            showValue(event) {
-                                this.peer.innerText = event.value;
-                            }
-                        }
-                    }
-                },
-                extend$conf: {
-                    zone: {
-                        border: {
-                            right: 6
-                        },
-                        cursor: {
-                            "TR": "ew-resize",
-                            "CR": "ew-resize",
-                            "BR": "ew-resize",
-                        },
-                        subject: {
-                            "TR": "size",
-                            "CR": "size",
-                            "BR": "size",
-                        }
-                    },	
-                },        
-                extend$actions: {
-                    showValue(event) {
-                        this.owner.send(this.parts.value, event);
-                    },
-                    size(event) {
-                        if (event.track == this) {
-                            let r = this.bounds;
-                            this.bounds = {
-                                width: event.clientX - r.left
-                            }
-                        }
-                    }        
-                }
-            },
-			main: {
+			type$context: "Display",
+			type$sidebar: "Sidebar",
+            main: {
                 type$: "Structure",
                 members: {
-                    tabs: {
-                        type$: "Display"
-                    },
+                    type$tabs: "Tabs",
+                    type$tables: "Display"
                 }
             }
 		}
     },
+    Tabs: {
+        type$: "Section",
+        members: {
+            header: {
+                type$: "Display"
+            },
+            body: {
+                type$: "Display"
+            }
+        }
+    },
+    Sidebar: {
+        type$: ["Structure", "Shape"],
+        members: {
+            tree: {
+                type$: "Collection",
+                type$contentType: "Item"
+            },
+            value: {
+                type$: "Display",
+                nodeName: "pre",
+                extend$actions: {
+                    showValue(event) {
+                        this.peer.innerText = event.value;
+                    }
+                }
+            }
+        },
+        extend$conf: {
+            zone: {
+                border: {
+                    right: 6
+                },
+                cursor: {
+                    "TR": "ew-resize",
+                    "CR": "ew-resize",
+                    "BR": "ew-resize",
+                },
+                subject: {
+                    "TR": "size",
+                    "CR": "size",
+                    "BR": "size",
+                }
+            },	
+        },        
+        extend$actions: {
+            showValue(event) {
+                this.owner.send(this.parts.value, event);
+            },
+            size(event) {
+                if (event.track == this) {
+                    let r = this.bounds;
+                    this.bounds = {
+                        width: event.clientX - r.left
+                    }
+                }
+            }
+        }
+    },
     ItemHeader: {
         type$: "Display",
-        type$isUpperCase: "/base/util/Text/isUpperCase",
         facets: {
             folder: {
                 icon: "/res/icons/folder-open.svg",
             },
-            pkg: {
-                icon: "/res/icons/archive.svg",
-            },
             file: {
                 icon: "/res/icons/file.svg",
+            },
+            pkg: {
+                icon: "/res/icons/archive.svg",
             },
             interface: {
                 icon: "/res/icons/gift.svg",
@@ -87,17 +107,7 @@ export default {
             },
             type: {
                 icon: "/res/icons/link.svg",
-            },
-
-            get: {
-                icon: "/res/icons/minus.svg",
-                title: true
-            },
-            virtual: {
-                icon: "/res/icons/plus.svg",
-                title: true
-            },
-    
+            }
         },
         states: {
             "collapsed": "/res/icons/chevron-right.svg",
@@ -179,24 +189,6 @@ export default {
         members: {
             type$header: "ItemHeader",
             type$body: "ItemBody"
-        },
-        view(model) {
-            if (!model) throw new Error();
-            this.super(view, model);
-            // let facet = this.facets[type];
-            // let ico = facet ? facet.icon : "/res/icons/flag.svg";
-            // let title = facet && facet.title ? type : "";
-
-            // this.peer.innerHTML = `<img> <img src="${ico}" title="${type}"> ${key} <i>${title}</i>`;
-
-            // if (!key) {
-            //     this.state = "hidden";
-            //     this.of.style.display = "none";
-            // } else if (model && typeof model.expr == "object") {
-            //     this.state = "collapsed";
-            // } else {
-            //     this.state = "empty";
-            // }
         },
 		extend$actions: {
             empty(event) {
