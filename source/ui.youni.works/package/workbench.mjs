b64d6819ef1f60dbd31b8b6eed055c0e948e3827
John Haley <jdhaley@gmail.com>
2021-07-14 13:05:17 -0400

    tabs somewhat working

--- source/ui.youni.works/package/workbench.mjs
+++ source/ui.youni.works/package/workbench.mjs
@@ -1,5 +1,6 @@
 export default {
 	type$: "/panel",
+    type$tree: "/tree",
     type$Shape: "/shape/Shape",
 	Workbench: {
         type$: "Section",
@@ -30,12 +31,52 @@ export default {
     },
     Tabs: {
         type$: "Section",
+        conf: {
+            type$tab: "Tab"
+        },
+        var$activeTab: null,
         members: {
             header: {
-                type$: "Display"
+                type$: "Collection"
             },
             body: {
                 type$: "Display"
+            }
+        },
+        add(title, body) {
+            let tab = this.owner.create(this.conf.tab);
+            tab.peer.innerText = title;
+            this.parts.header.append(tab);
+            if (!this.activeTab) this.activeTab = tab;
+            return tab;
+        },
+        display() {
+            this.super(display);
+            this.add("Tree");
+            this.add("Other");
+            this.add("Other");
+            this.add("Other");
+            this.add("Other");
+            this.add("Other");
+            this.add("Other");
+            this.add("Other");
+        },
+        extend$actions: {
+            showTab(event) {
+                this.activeTab.style.removeProperty("background");
+                this.activeTab.style.removeProperty("color");
+                this.activeTab = event.tab;
+                this.activeTab.style.background = "white";
+                this.activeTab.style.color = "darkslategray"
+            }
+        }
+    },
+    Tab: {
+        type$: "Display",
+        extend$actions: {
+            click(event) {
+                event.subject = "showTab";
+                event.tab = this;
             }
         }
     },
@@ -44,7 +85,7 @@ export default {
         members: {
             tree: {
                 type$: "Collection",
-                type$contentType: "Item"
+                type$contentType: "tree/Item"
             },
             value: {
                 type$: "Display",
@@ -87,124 +128,4 @@ export default {
             }
         }
     },
-    ItemHeader: {
-        type$: "Display",
-        facets: {
-            folder: {
-                icon: "/res/icons/folder-open.svg",
-            },
-            file: {
-                icon: "/res/icons/file.svg",
-            },
-            pkg: {
-                icon: "/res/icons/archive.svg",
-            },
-            interface: {
-                icon: "/res/icons/gift.svg",
-            },
-            method: {
-                icon: "/res/icons/settings.svg",
-            },
-            type: {
-                icon: "/res/icons/link.svg",
-            }
-        },
-        states: {
-            "collapsed": "/res/icons/chevron-right.svg",
-            "expanded": "/res/icons/chevron-bottom.svg",
-            "empty": "/res/icons/empty.svg",
-            "hidden": ""
-        },
-        virtual$state(value) {
-            if (!arguments.length) return this.peer.$state;
-            this.peer.$state = value;
-            this.peer.firstChild.src = this.states[value];
-        },
-        view(model) {
-            this.super(view, model);
-            if (!model) {
-                console.log(this.of.key);
-            }
-            let facet = this.facets[model.facet];
-            let icon = facet ? facet.icon : "/res/icons/fullscreen.svg";
-            let type = model.type;
-            let value = "";
-            switch (type) {
-                case "function":
-                case "object":
-                    type = "";
-                    break;
-                case "number":
-                    type = "";
-                    value = model.expr;
-                    icon = "/res/icons/fullscreen-exit.svg";
-                    break;
-                case "string":
-                    type = "";
-                    value = model.expr;
-                    icon = "/res/icons/tag.svg";
-                    break;
-                case "boolean":
-                    type = "";
-                    value = model.expr ? true : false;
-                    icon = "/res/icons/flag.svg";
-                    break;
-            }
-            this.peer.innerHTML = `<img> <img src="${icon}" title="${model.facet}"> ${model.name} <i>${type}</i> <span>${value}</span>`;
-
-            if (typeof model.expr == "object") {
-                this.state = "collapsed";
-            } else {
-                this.state = "empty";
-            }
-        },
-        extend$actions: {
-            click(event) {
-                event.value = this.model.expr;
-                if (this.state === "collapsed") {
-                    this.state = "expanded";
-                } else if (this.state == "expanded") {
-                    this.state = "collapsed";
-                }
-                event.subject = this.state;
-            }
-        }
-    },
-    ItemBody: {
-        type$: "Collection",
-        type$contentType: "Item",
-        view(model) {
-            if (this.peer.$show) {
-                // let content = model;
-                // if (content.expr && typeof content.expr == "object") content = model.expr;
-                this.super(view, model.expr);
-                this.owner.send(this, "view");
-            } else {
-                this.peer.$show = true;
-            }
-        }
-    },
-    Item: {
-		type$: "Structure",
-        members: {
-            type$header: "ItemHeader",
-            type$body: "ItemBody"
-        },
-		extend$actions: {
-            empty(event) {
-                event.subject = "showValue";
-            },
-			collapsed(event) {
-                this.parts.body.style.display = "none";
-                event.subject = "";
-			},
-			expanded(event) {
-                if (!this.parts.body.peer.childNodes.length) {
-                    this.parts.body.view(this.model);
-                }
-                this.parts.body.style.removeProperty("display");
-                event.subject = "";
-			}
-		}
-    }
 }
