21a862b4cb4da66cc46b4b436403921f0a0ddced
jdh <jdhaley@gmail.com>
2022-03-23 12:37:05 -0400

    module/noted for noted-contributed modules.

--- main.mjs
+++ main.mjs
@@ -2,7 +2,7 @@ import express	from "express";
 import fs		from "fs";
 import http	from "http";
 import bodyParser	from "body-parser";
-import start	from "./module/service/start.js";
+import start	from "./module/noted/service/start.js";
 start({
 	express: express,
 	bodyParser: bodyParser,
