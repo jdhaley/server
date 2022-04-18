9346a8e4504d906d76f0c24d3b66b21a961a7f75
jdh <jdhaley@gmail.com>
2022-04-17 21:10:00 -0400

    service now driven by core/service

--- main.mjs
+++ main.mjs
@@ -2,10 +2,19 @@ import express	from "express";
 import fs		from "fs";
 import http	from "http";
 import bodyParser	from "body-parser";
-import start	from "./module/noted/service/start.js";
-start({
-	express: express,
-	bodyParser: bodyParser,
-	fs: fs,
-	http: http
-});
+
+import start from "./module/core/service/service.js"
+import serviceConf	from "./module/noted/service/conf.js";
+
+const serverConf = {
+	server: {
+		port: 4040,
+	},
+	modules: {
+		express: express,
+		bodyParser: bodyParser,
+		fs: fs,
+		http: http
+	}
+}
+start(serverConf, serviceConf);
