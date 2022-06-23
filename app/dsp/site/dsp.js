76c6f518f42277f4ecc9c03aa3f8b687a956af03
jdh <jdhaley@gmail.com>
2022-06-23 14:03:42 -0400

    updates from youni changes.

--- app/dsp/site/dsp.js
+++ app/dsp/site/dsp.js
@@ -1,6 +1,6 @@
 
 import {Frame} from "./youni.works/ui/ui.js";
-import {Article} from "./youni.works/ui/editor/article.js";
+import {Article} from "./youni.works/ui/article.js";
 import conf from "./properties.js";
 
 let frame = new Frame(window, conf.controllers.frame);
