a0c4ecef09a8c25acd25fe6b9d4d22620dc16395
jdh <jdhaley@gmail.com>
2022-06-23 16:29:26 -0400

    comment out noted, core, lang applications from configuration.

--- endpoints.js
+++ endpoints.js
@@ -1,22 +1,22 @@
 import rfs from "./module/youni.works/v2/service/actions/rfs.js";
 export default {
 	//For viewing .ts only
-	"/noted": "../noted",
-	"/core": "../core",
-	"/lang": "../lang",
+	// "/noted": "../noted",
+	// "/core": "../core",
+	// "/lang": "../lang",
 	"/youni.works": "../youni.works",
 
 	//For all apps
 	"/module": "module",
 	"/journal": rfs,
 
-	//Src app
-	"/src": "../lang/app/site",
-	"/src/app": "../lang/app/site/src.html",
+	// //Src app
+	// "/src": "../lang/app/site",
+	// "/src/app": "../lang/app/site/src.html",
 
-	//Testing
-	"/test": "../lang/test/site",
-	"/test/app": "../lang/test/site/test.html",
+	// //Testing
+	// "/test": "../lang/test/site",
+	// "/test/app": "../lang/test/site/test.html",
 
 	//Dsp
 	"/dsp": "app/dsp/site",
