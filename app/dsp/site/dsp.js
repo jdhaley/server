b285fdd135240a7a3cd58d06bd445be81d4da06c
jdh <jdhaley@gmail.com>
2022-07-05 16:05:38 -0400

    using Editor rather than Article.

--- app/dsp/site/dsp.js
+++ app/dsp/site/dsp.js
@@ -1,9 +1,10 @@
 
-import {Frame, Article} from "./youni.works/ui/ui.js";
+import {Frame} from "./youni.works/ui/ui.js";
+import {Editor} from "./youni.works/ui/editor/edit.js"
 import conf from "./properties.js";
 
 let frame = new Frame(window, conf.controllers.frame);
-let article = new Article(frame, conf);
+let article = new Editor(frame, conf);
 
 let articlePath = "/" + frame.location.search.substring(1);
 
