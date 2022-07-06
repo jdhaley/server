98c762579552bbcc0e5f85d359bbd186b13d7fcb
jdh <jdhaley@gmail.com>
2022-07-06 11:31:25 -0400

    Editor - now Article.

--- app/dsp/site/dsp.js
+++ app/dsp/site/dsp.js
@@ -1,10 +1,10 @@
 
 import {Frame} from "./youni.works/ui/ui.js";
-import {Editor} from "./youni.works/ui/editor/edit.js"
-import conf from "./properties.js";
+import {Article} from "./youni.works/ui/editor/edit.js"
+import conf from "./conf.js";
 
 let frame = new Frame(window, conf.controllers.frame);
-let article = new Editor(frame, conf);
+let article = new Article(frame, conf);
 
 let articlePath = "/" + frame.location.search.substring(1);
 
