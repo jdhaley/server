01ba688bab4f6559a79279ae55aec874a90a301b
jdh <jdhaley@gmail.com>
2022-07-22 06:11:17 -0400

    EDITORS configured.

--- app/dsp/site/conf.js
+++ app/dsp/site/conf.js
@@ -1,10 +1,17 @@
-import baseTypes from "./youni.works/ui/conf/types.js";
-import viewTypes from "./views.js";
-
 import article from "./youni.works/ui/controllers/article.js";
 import frame from "./youni.works/ui/controllers/frame.js";
 
+import viewers from "./youni.works/ui/conf/viewers.js";
+import modellers from "./youni.works/ui/conf/modellers.js";
+import editors from "./youni.works/ui/conf/editors.js";
+
+import baseTypes from "./youni.works/ui/conf/types.js";
+import viewTypes from "./views.js";
+
 export default {
+	viewers: viewers,
+	modellers: modellers,
+	editors: editors,
 	actions: {
 		frame: frame,
 		article: article
