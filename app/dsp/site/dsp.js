f0f19be3bdd067f88b3f5db1d3dfdd4b3351d59e
jdh <jdhaley@gmail.com>
2022-08-06 07:32:00 -0400

    Article is now an interface - change to DisplayOwner instead.

--- app/dsp/site/dsp.js
+++ app/dsp/site/dsp.js
@@ -1,13 +1,13 @@
 
 import {Frame} from "./youni.works/ui/ui.js";
-import {Article} from "./youni.works/ui/article.js"
+import {DisplayOwner} from "./youni.works/ui/display.js"
 import controller from "./youni.works/ui/controllers/frame.js";
 
 import conf from "./conf.js";
 
 let frame = new Frame(window, controller);
-let article = new Article(frame, conf);
+let display = new DisplayOwner(frame, conf);
 
-let articlePath = "/" + frame.location.search.substring(1);
+let filePath = "/" + frame.location.search.substring(1);
 
-article.service.open(articlePath, article);
+display.service.open(filePath, display);
