f4f712c6dcbbe14b1e48e9b499b9cf72e0d54883
jdh <jdhaley@gmail.com>
2022-09-23 15:06:06 -0400

    Frame and Display in ui.ts

--- app/dsp/site/dsp.js
+++ app/dsp/site/dsp.js
@@ -1,12 +1,11 @@
 
-import {Frame} from "./youni.works/ui/ui.js";
-import {DisplayOwner} from "./youni.works/ui/display/view.js"
+import {Frame, Display} from "./youni.works/ui/ui.js";
 import controller from "./youni.works/ui/controllers/frame.js";
 
 import conf from "./conf.js";
 
 let frame = new Frame(window, controller);
-let display = new DisplayOwner(frame, conf);
+let display = new Display(frame, conf);
 
 let filePath = "/" + frame.location.search.substring(1);
 
