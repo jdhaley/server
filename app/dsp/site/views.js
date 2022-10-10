94ae2c65ea5add3ad5c90d270cea3e7da38f217b
jdh <jdhaley@gmail.com>
2022-10-10 09:01:56 -0400

    added workTask type in views

--- app/dsp/site/views.js
+++ app/dsp/site/views.js
@@ -94,13 +94,68 @@ export default {
 			heading: {
 				type: "line"
 			},
-			row: "row"
+			worktask: "worktask"
 		}
 	},
 	row: "row",
-	column: {
+	cell: {
 		type: "text",
 		container: false,
 		tagName: "ui-cell"
+	},
+	//Task model: Category: Title/Short Description: Due Date Time: : Status: Priority (optional): Assigned To (optional)
+	worktask: {
+		type: "row",
+		title: "Task",
+		tableTitle: "Tasks",
+		types: {
+			category: {
+				type: "cell",
+				title: "Category"
+			},
+			title: {
+				type: "cell",
+				title: "Title",
+			},
+			due: {
+				type: "cell",
+				title: "Due"
+			},
+			status: {
+				type: "cell",
+				title: "Status"
+			},
+			priority: {
+				type: "cell",
+				title: "Priority"
+			},
+			owner: {
+				type: "cell",
+				title: "Owner"
+			}
+		}
+	},
+	//Work log model: Start Date Time to End Date Time: Category: Title/Short Description
+	worklog: {
+		type: "row",
+		title: "Log",
+		types: {
+			startDate: {
+				type: "text",
+				title: "From"
+			},
+			endDate: {
+				type: "text",
+				title: "From"
+			},
+			category: {
+				type: "text",
+				title: "Category"
+			},
+			title: {
+				type: "text",
+				title: "Title",
+			}	
+		}
 	}
 }
