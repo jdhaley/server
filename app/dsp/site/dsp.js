9775a06885b17db7a40de2d17ca3640f0b45f99e
jdh <jdhaley@gmail.com>
2022-11-12 10:09:56 -0500

    dsp.js updated with new location for Frame & ElementArticle

--- app/dsp/site/dsp.js
+++ app/dsp/site/dsp.js
@@ -1,13 +1,13 @@
 
-import { Frame } from "./youni.works/ui/frame.js";
-import { Display } from "./youni.works/ui/box.js";
+import { Frame } from "./youni.works/control/frame.js";
+import { ElementArticle } from "./youni.works/control/view.js";
 
 import controller from "./youni.works/ui/actions/frame.js";
 
 import conf from "./conf.js";
 
 let frame = new Frame(window, controller);
-let display = new Display(frame, conf);
+let display = new ElementArticle(frame, conf);
 
 let filePath = "/" + frame.location.search.substring(1);
 
