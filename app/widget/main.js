7a6c403ef4ed76e003a53667e321db2b4788801c
jdh <jdhaley@gmail.com>
2022-11-15 22:33:18 -0500

    widget devt

--- app/widget/main.js
+++ app/widget/main.js
@@ -1,10 +1,11 @@
 
+import { Frame } from "./youni.works/control/frame.js";
 import { ElementView } from "./youni.works/ui/widgets/display.js";
 
-// let header = {
-// 	kind: "caption",
-// 	content: (props) => `The title is "${props.title}".`
-// }
+import controller from "./youni.works/ui/actions/frame.js";
+import shape from "./youni.works/ui/actions/shape.js";
+
+new Frame(window, controller);
 
 let test = {
 	kind: "test",
@@ -13,11 +14,51 @@ let test = {
 	},
 	header: {
 		kind: "caption",
-		content: "Test thing"
+		content: "Test thing",
 	},
 	content: "test content",
+	actions: shape
+}
+
+new ElementView(document.body, test);
+
+let person = {
+	kind: "person",
+	prop: {
+		title: "Person"
+	},
+	header: {
+		kind: "caption",
+		content: "Person"
+	},
+	content: {
+		firstName: {
+			header: {
+				kind: "label",
+				content: () => "<b>F</b>irst <b>N</b>ame"
+			},
+			content: "Billy"
+		},
+		lastName: {
+			header: {
+				kind: "label",
+				content: "Last Name"
+			},
+			content: "Willy"
+		},
+		dob: {
+			header: {
+				kind: "label",
+				content: "Date of Birth"
+			},
+			content: new Date().toISOString()
+		}
+	},
+	footer: {
+		content: "A person is a person no matter who small."
+	},
 	actions: {
 	}
 }
 
-new ElementView(document.body, test);
+new ElementView(document.body, person);
