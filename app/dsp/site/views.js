dc2db75fab1f890171df5cc03005f26c1b2af8b0
jdh <jdhaley@gmail.com>
2022-12-10 09:38:54 -0500

    comment out unused types in views.js

--- app/dsp/site/views.js
+++ app/dsp/site/views.js
@@ -82,13 +82,6 @@ export default {
 			}
 		}
 	},
-	tasks: {
-		type: "list",
-		title: "Tasks",
-		types: {
-			task: "task"
-		}
-	},
 	unknown: "text",
 	person: {
 		type: "record",
@@ -139,68 +132,75 @@ export default {
 			heading: {
 				type: "line"
 			},
-			worktask: "worktask"
+			//worktask: "worktask"
 		}
 	},
-	row: "row",
-	cell: {
-		type: "text",
-		container: false,
-		tagName: "ui-cell"
-	},
-	//Task model: Category: Title/Short Description: Due Date Time: : Status: Priority (optional): Assigned To (optional)
-	worktask: {
-		type: "row",
-		title: "Task",
-		tableTitle: "Tasks",
-		types: {
-			category: {
-				type: "cell",
-				title: "Category"
-			},
-			title: {
-				type: "cell",
-				title: "Title",
-			},
-			due: {
-				type: "cell",
-				title: "Due"
-			},
-			status: {
-				type: "cell",
-				title: "Status"
-			},
-			priority: {
-				type: "cell",
-				title: "Priority"
-			},
-			owner: {
-				type: "cell",
-				title: "Owner"
-			}
-		}
-	},
-	//Work log model: Start Date Time to End Date Time: Category: Title/Short Description
-	worklog: {
-		type: "row",
-		title: "Log",
-		types: {
-			startDate: {
-				type: "text",
-				title: "From"
-			},
-			endDate: {
-				type: "text",
-				title: "From"
-			},
-			category: {
-				type: "text",
-				title: "Category"
-			},
-			title: {
-				type: "text",
-				title: "Title",
-			}	
-		}
-	}
+	// row: "row",
+	// cell: {
+	// 	type: "text",
+	// 	container: false,
+	// 	tagName: "ui-cell"
+	// },
+	// tasks: {
+	// 	type: "list",
+	// 	title: "Tasks",
+	// 	types: {
+	// 		task: "task"
+	// 	}
+	// },
+	// //Task model: Category: Title/Short Description: Due Date Time: : Status: Priority (optional): Assigned To (optional)
+	// worktask: {
+	// 	type: "row",
+	// 	title: "Task",
+	// 	tableTitle: "Tasks",
+	// 	types: {
+	// 		category: {
+	// 			type: "cell",
+	// 			title: "Category"
+	// 		},
+	// 		title: {
+	// 			type: "cell",
+	// 			title: "Title",
+	// 		},
+	// 		due: {
+	// 			type: "cell",
+	// 			title: "Due"
+	// 		},
+	// 		status: {
+	// 			type: "cell",
+	// 			title: "Status"
+	// 		},
+	// 		priority: {
+	// 			type: "cell",
+	// 			title: "Priority"
+	// 		},
+	// 		owner: {
+	// 			type: "cell",
+	// 			title: "Owner"
+	// 		}
+	// 	}
+	// },
+	// //Work log model: Start Date Time to End Date Time: Category: Title/Short Description
+	// worklog: {
+	// 	type: "row",
+	// 	title: "Log",
+	// 	types: {
+	// 		startDate: {
+	// 			type: "text",
+	// 			title: "From"
+	// 		},
+	// 		endDate: {
+	// 			type: "text",
+	// 			title: "From"
+	// 		},
+	// 		category: {
+	// 			type: "text",
+	// 			title: "Category"
+	// 		},
+	// 		title: {
+	// 			type: "text",
+	// 			title: "Title",
+	// 		}	
+	// 	}
+	//}
 }
